6241e1b6056ce3d5905698d0d98c6ea518e99508
Ramgopal Mangena <ramgopal.mangena@gmail.com>
2016-09-27 13:21:55 -0500

    Updated home page
    Updated home file with recent changes

--- Source/OneStopForCS/app/src/main/java/com/example/malin/onestopforcs/Home.java
+++ Source/OneStopForCS/app/src/main/java/com/example/malin/onestopforcs/Home.java
@@ -1,7 +1,10 @@
 package com.example.malin.onestopforcs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 /**
  * Created by malin on 23-09-2016.
@@ -10,6 +13,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        Intent intent = getIntent();
+        if(intent != null && !TextUtils.isEmpty(intent.getStringExtra(LoginActivity.NAME))) {
+            Toast.makeText(Home.this, intent.getStringExtra(LoginActivity.NAME), Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
